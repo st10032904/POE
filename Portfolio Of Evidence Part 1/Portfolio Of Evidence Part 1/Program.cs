f20619370cf004ac8848db5ca0ea02ca7c09a504
Ali Khazem <ST10032904@vcconnect.edu.za>
2023-04-26 15:18:53 +0200

    While loop and basic formatting completed

---
+++ Portfolio Of Evidence Part 1/Portfolio Of Evidence Part 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio_Of_Evidence_Part_1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiating ArrayList objects
+            ArrayList Ingredient = new ArrayList();
+            ArrayList Quantity = new ArrayList();
+            ArrayList unitOfMeasurement = new ArrayList();
+
+
+            int numberOfIngredients = Convert.ToInt32(Console.ReadLine("How many ingredients are there going to be in this recipe?"));
+            while (numberOfIngredients > 0)
+            {
+                Console.WriteLine("Please enter the name of the ingredient?");
+                String ingredient = Console.ReadLine();
+                Ingredient.Add(ingredient);
+                Console.WriteLine("Please enter the quantity for this ingredient");
+                Double quantity = Convert.ToDouble(Console.ReadLine());
+                Quantity.Add(quantity);
+                Console.WriteLine("Please enter the unit of measurement for this ingredient");
+                String unit = Console.ReadLine();
+                unitOfMeasurement.Add(unit);
+                numberOfIngredients--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
