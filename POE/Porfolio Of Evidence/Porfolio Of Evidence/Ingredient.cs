0dc9aa60b46386e2bef62e5d0f663d31eb11d343
Ali Khazem <ST10032904@vcconnect.edu.za>
2023-06-05 17:27:29 +0200

    Ported code over from previous commit to Ingredient Class

--- POE/Porfolio Of Evidence/Porfolio Of Evidence/Ingredient.cs
+++ POE/Porfolio Of Evidence/Porfolio Of Evidence/Ingredient.cs
@@ -8,5 +8,8 @@ namespace Porfolio_Of_Evidence
 {
     internal class Ingredient
     {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
     }
 }
