d67128ef9a78b0f1c09b91cba3f5b35f86342212
Ali Khazem <ST10032904@vcconnect.edu.za>
2023-06-05 17:26:11 +0200

    Re-created Recipe Class and Ingredient Class; ported over code from previous commit to recipe class

---
+++ POE/Porfolio Of Evidence/Porfolio Of Evidence/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porfolio_Of_Evidence
+{
+    internal class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<string> Steps { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+            Steps = new List<string>();
+        }
+    }
+}
